d539ae04875e3158460f1cc0ce4e859a3f6a9717
Clayt <clayton.wwjd@gmail.com>
2020-03-18 11:26:27 -0500

    Sneaky sneaky subdomain

--- pullWarchests.py
+++ pullWarchests.py
@@ -2,8 +2,8 @@ import json
 import requests
 import time
 
-a = requests.get('https://camelotpnw.tech/api/v1/members?page=1')
-b = requests.get('https://camelotpnw.tech/api/v1/members?page=2')
+a = requests.get('http://camnet.camelotpnw.tech/api/v1/members?page=1')
+b = requests.get('http://camnet.camelotpnw.tech/api/v1/members?page=2')
 
 with open('Camelot_Warchest_{}.json'.format(time.strftime("%Y%m%d-%H%M%S")), 'w') as my_data_file:
     json.dump(a.json(), my_data_file)
