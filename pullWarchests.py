4d5ba42af85e5f7f7c9b7397cdf068cf5d036479
Clayt <clayton.wwjd@gmail.com>
2020-03-31 19:38:07 -0500

    Three pages.... oh my.... ;)

--- pullWarchests.py
+++ pullWarchests.py
@@ -4,7 +4,9 @@ import time
 
 a = requests.get('http://camnet.camelotpnw.tech/api/v1/members?page=1')
 b = requests.get('http://camnet.camelotpnw.tech/api/v1/members?page=2')
+c = requests.get('http://camnet.camelotpnw.tech/api/v1/members?page=3')
 
 with open('Camelot_Warchest_{}.json'.format(time.strftime("%Y%m%d-%H%M%S")), 'w') as my_data_file:
     json.dump(a.json(), my_data_file)
     json.dump(b.json(), my_data_file)
+    json.dump(c.json(), my_data_file)
